bbec53e1552db123adff5d4417c91bc9919eb550
Hugo Atlan <hugoat2003@gmail.com>
2022-07-06 17:25:26 +0200

    version 0.1

---
+++ pion.py
@@ -0,0 +1,15 @@
+import pygame.sprite
+
+
+class Pion(pygame.sprite.Sprite):
+
+    def __init__(self, ID, TYPE, COLOR):
+        self.__init__()
+        self.id = ID
+        self.type = TYPE
+        self.color = COLOR
+        self.image = pygame.image.load(IMAGE)
+        self.rect = self.image.get_rect()
+        self.rect.x = POSITION[0]
+        self.rect.x = POSITION[1]
+
